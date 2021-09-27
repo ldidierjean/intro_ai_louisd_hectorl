069a71e7d090b13eb7a7d17345ddcd8eaa623fe5
Louis <louisdidierjean@yahoo.fr>
2021-09-27 22:30:32 +0200

    Add requirements and color mappings

---
+++ hector_src/mappings.py
@@ -0,0 +1,8 @@
+color_mappings = {"pink": 0,
+          "blue": 1,
+          "purple": 2,
+          "grey": 3,
+          "white": 4,
+          "black": 5,
+          "red": 6,
+          "brown": 7}
