57dd8eb11e6a2dd62d20d39206602e0ab9896274
Hector <hlevresse@gmail.com>
2021-09-30 00:34:31 +0200

    .

--- hector_src/state.py
+++ hector_src/state.py
@@ -1,6 +1,7 @@
 from typing import Dict, Tuple, List, Set
 from globals import PlayerType, CharacterColor, color_mappings, question_mappings
 from Character import Character
+import copy
 
 def generate_state_from_server_question(question: Dict, player_type: PlayerType):
     game_state: Dict = question['game state']
@@ -11,10 +12,10 @@ def generate_state_from_server_question(question: Dict, player_type: PlayerType)
     for character in game_state['characters']:
         if character['suspect'] == True:
             suspects.add(color_mappings[character['color']])
-    
+
     for card in game_state['active character_cards']:
         active_cards.add(color_mappings[card['color']])
-    
+
     state = State(
         color_mappings[game_state['fantom']] if 'fantom' in game_state else -1,
         game_state['position_carlotta'],
@@ -66,12 +67,53 @@ class State:
         self.choose_to_reach_state = choose_to_reach_state
         self.question = question
 
-    def generate_state(self):
+    def __repr__(self):
+        return "Fantom: " + str(self.fantom) + \
+            "\npos_carlotta: " + str(self.pos_carlotta) + \
+            "\nnbr_turn: " + str(self.nbr_turn) + \
+            "\nnext_player: " + str(self.next_player) + \
+            "\nshadow: " + str(self.shadow) + \
+            "\nblocked: " + str(self.blocked) + \
+            "\nsuspect: " + str(self.suspect) + \
+            "\nactive_cards: " + str(self.active_cards) + \
+            "\nchoose_to_reach_state: " + str(self.choose_to_reach_state) + \
+            "\nquestion: " + str(self.question)
 
+
+    def generate_state(self):
+        switcher = {
+            0: self.select_character,
+            1: self.activate_white_power,
+            2: self.activate_purple_power,
+            3: self.activate_brown_power,
+            4: self.activate_grey_power,
+            5: self.activate_blue_power,
+            6: self.activate_pink_power,
+            7: self.activate_black_power,
+            8: self.activate_red_power,
+            9: self.select_position,
+            10: self.purple_character_power,
+            11: self.brown_character_power,
+            12: self.grey_character_power,
+            13: self.blue_character_power_room,
+            14: self.blue_character_power_exit,
+            15: self.white_character_power_move_purple,
+            16: self.white_character_power_move_brown,
+            17: self.white_character_power_move_grey,
+            18: self.white_character_power_move_blue,
+            19: self.white_character_power_move_pink,
+            20: self.white_character_power_move_black,
+            21: self.white_character_power_move_red
+        }
+        switcher.get(self.question)()
         return self
 
     def select_character(self):
         print('select_character:')
+        nextStates = []
+        for c in self.active_cards:
+            copy = self.copy()
+            nextStates.append()
         return self
 
     def activate_white_power(self):
