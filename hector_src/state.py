d66ee70714c122ddf542bf8e5f95df498fcc83ea
Hector <hlevresse@gmail.com>
2021-09-30 00:05:45 +0200

    removed ongoing card

--- hector_src/state.py
+++ hector_src/state.py
@@ -27,7 +27,6 @@ class State:
     blocked: Tuple[int, int]
     suspect: Set[int]
     active_cards: Set[int]
-    ongoing_card: Character
     choose_to_reach_state: int
     question: int
 
@@ -41,7 +40,6 @@ class State:
             blocked: Tuple[int, int],
             suspect: Set[int],
             active_cards: Set[int],
-            oc: Character,
             choose_to_reach_state: int,
             question: int
     ):
@@ -53,10 +51,97 @@ class State:
         self.blocked = blocked
         self.suspect = suspect
         self.active_cards = active_cards
-        self.ongoing_card = Character(oc.color, oc.suspect, oc.position, oc.power_used)
         self.choose_to_reach_state = choose_to_reach_state
         self.question = question
 
     def generate_state(self):
+
         return self
 
+    def select_character(self):
+        print('select_character:')
+        return self
+
+    def activate_white_power(self):
+        print('activate_white_power:')
+        return self
+
+    def activate_purple_power(self):
+        print('activate_purple_power:')
+        return self
+
+    def activate_brown_power(self):
+        print('activate_brown_power:')
+        return self
+
+    def activate_grey_power(self):
+        print('activate_grey_power:')
+        return self
+
+    def activate_blue_power(self):
+        print('activate_blue_power:')
+        return self
+
+    def activate_pink_power(self):
+        print('activate_pink_power:')
+        return self
+
+    def activate_black_power(self):
+        print('activate_black_power:')
+        return self
+
+    def activate_red_power(self):
+        print('activate_red_power:')
+        return self
+
+    def select_position(self):
+        print('select_position:')
+        return self
+
+    def purple_character_power(self):
+        print('purple_character_power:')
+        return self
+
+    def brown_character_power(self):
+        print('brown_character_power:')
+        return self
+
+    def grey_character_power(self):
+        print('grey_character_power:')
+        return self
+
+    def blue_character_power_room(self):
+        print('blue_character_power_room:')
+        return self
+
+    def blue_character_power_exit(self):
+        print('blue_character_power_exit:')
+        return self
+
+    def white_character_power_move_purple(self):
+        print('white_character_power_move_purple:')
+        return self
+
+    def white_character_power_move_brown(self):
+        print('white_character_power_move_brown:')
+        return self
+
+    def white_character_power_move_grey(self):
+        print('white_character_power_move_grey:')
+        return self
+
+    def white_character_power_move_blue(self):
+        print('white_character_power_move_blue:')
+        return self
+
+    def white_character_power_move_pink(self):
+        print('white_character_power_move_pink:')
+        return self
+
+    def white_character_power_move_black(self):
+        print('white_character_power_move_black:')
+        return self
+
+    def white_character_power_move_red(self):
+        print('white_character_power_move_red:')
+        return self
